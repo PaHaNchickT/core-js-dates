147a50963c894635367836be4fe2f9304aab1e89
Pavel <you@example.com>
2024-03-01 14:44:07 +0300

    feat: implement isLeapYear task (01.03.24 14:44)

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -312,8 +312,9 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (new Date(`${date.getFullYear()}-02-29`).getMonth() === 1) return true;
+  return false;
 }
 
 module.exports = {
